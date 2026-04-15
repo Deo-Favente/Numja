45d28b6f0dee55c167200e54a83e7d707e75e4b6
Loic GIRAUD <lgiraud.giraud@gmail.com>
2026-04-15 09:56:47 +0200

    testing lower coverage again

--- numja/src/test/java/fr/uga/numja/NdarrayTest.java
+++ numja/src/test/java/fr/uga/numja/NdarrayTest.java
@@ -17,33 +17,6 @@ public class NdarrayTest {
         Ndarray.zeros(1, 0);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testCreateZerosNdarray2DInvalidRows(){
-        Ndarray.zeros(-1, 10);
-    }
-
-    @Test
-    public void testCreateZerosNdarry1D(){
-        Ndarray array = Ndarray.zeros(10);
-        int[] shape = new int[2];
-        shape[0] = 10;
-        shape[1] = 0;
-        assertEquals("Created array should have size", 10, array.getSize());
-        assertEquals("Created array should have dimension ", 1, array.getNdim());
-        assertArrayEquals("Created array should have the shape", shape, array.getShape());
-    }
-
-    @Test
-    public void testCreateZerosNdarry2D(){
-        Ndarray array = Ndarray.zeros(10, 10);
-        int[] shape = new int[2];
-        shape[0] = 10;
-        shape[1] = 10;
-        assertEquals("Created array should have size", 100, array.getSize());
-        assertEquals("Created array should have dimension ", 2, array.getNdim());
-        assertArrayEquals("Created array should have the shape", shape, array.getShape());
-    }
-
 
 
 }
